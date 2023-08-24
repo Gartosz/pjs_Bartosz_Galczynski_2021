a3295900c81ea60106edcdda5db7389bf8417d66
Bartosz <bartekg001@gmail.com>
2023-08-24 16:04:58 +0200

    Remove not needed bot instance from extension.

--- cointoss.py
+++ cointoss.py
@@ -1,14 +1,13 @@
 import random
 from discord.ext import commands
 
-bot = commands.Bot(command_prefix='=')
 
 
 class cointoss(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
 
-    @bot.command(name='cointoss', aliases=['moneta'], description='Losuje reszkę lub orła')
+    @commands.command(name='cointoss', aliases=['moneta'], description='Losuje reszkę lub orła')
     async def coin_toss(self, ctx):
         x = ['Wypadła reszka', 'Wypadł orzeł']
         side = random.choice(x)

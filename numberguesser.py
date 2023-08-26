d2af86369351901012bd273b562e6c5da21c8054
Bartosz <bartekg001@gmail.com>
2023-08-26 13:21:55 +0200

    Rename variable and remove unnecessary brackets.

--- numberguesser.py
+++ numberguesser.py
@@ -11,21 +11,21 @@ class NumberGuesser(commands.Cog):
                       description='Umożliwia zgadnięcie wylosowanej liczby całkowitej od 0 do 10,'
                                   ' bądź z podanego zakresu')
     async def guess(self, ctx, a=0, b=10):
-
         await ctx.send("Musisz zgadnąć liczbę z zakresu od " + str(a) + " do " + str(b))
 
         x = random.randint(a, b)
         number = a - 1
         i = 0
-        while (number != x):
-            if (number > b or number < a) and i != 0 and t:
+        while number != x:
+            if (number > b or number < a) and i != 0 and type_error:
                 await ctx.send("Podałeś liczbę spoza zakresu!")
-            elif i != 0 and t:
+            elif i != 0 and type_error:
                 await ctx.send("Nie zgadłeś/aś")
             i += 1
-            t = 1
+            type_error = 1
             try:
-                message = await self.bot.wait_for('message', check=lambda m: m.author == ctx.author, timeout=15.0)
+                message = await self.bot.wait_for('message', check=lambda msg: msg.author == ctx.author,
+                                                  timeout=15.0)
             except asyncio.TimeoutError:
                 await ctx.send('Niestety, skończył ci się czas :(')
                 return
@@ -34,7 +34,7 @@ class NumberGuesser(commands.Cog):
             except ValueError:
                 await ctx.send("Podałeś zły typ danych")
                 i -= 1
-                t = 0
+                type_error = False
 
         await ctx.send("Zgadłeś/aś liczbę " + str(x) + " po " + str(i) + " próbach.")
 
